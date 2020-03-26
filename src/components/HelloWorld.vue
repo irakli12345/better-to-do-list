233fce57c0c46aa48b5355cb9bcd8a57a23d336f
Irakli Tchigladze <dzagli12345@gmail.com>
2020-03-26 20:37:35 +0400

    sorted out fontAwesome icons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,16 @@
     <div class="lead-text-wrapper">
       <h1 class="leading-text">{{ msg }}</h1>
     </div>
-    <div class="todos"></div>
+    <div class="todos">
+      <div class="todo">
+        <div class="heading">
+          <i class="fas fa-minus-circle"></i>
+          <p>bla bla bla heading title</p>
+          <i class="far fa-calendar"></i>
+        </div>
+      </div>
+      <div class="todo"></div>
+    </div>
   </div>
 </template>
 
@@ -39,6 +48,39 @@ export default {
   margin: auto;
   margin-top: 2rem;
   border-radius: 4rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.todo {
+  flex-basis: 40%;
+  height: 60%;
+  background-color: rgba(42, 70, 255, 0.5);
+  border-radius: 2rem;
+}
+.fa-minus-circle {
+  background: rgba(255, 0, 0, 0.63);
+  border-radius: 50%;
+  border: 1px solid rgba(255, 0, 0, 0.63);
+}
+.fa-minus-circle:hover {
+  background: red;
+  border: 1px solid red;
+}
+.heading {
+  border-bottom: 2px solid red;
+  padding: 0.3rem 1.5rem;
+  max-height: 15%;
+  display: flex;
+  justify-content: space-between;
+}
+.heading p {
+  font-size: 12px;
+  font-weight: bold;
+  color: white;
+  max-width: 40%;
+  overflow: hidden;
+  max-height: 1rem;
 }
 @media screen and (min-width: 720px) {
   .leading-text {
